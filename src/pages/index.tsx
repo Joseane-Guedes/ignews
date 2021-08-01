68070d4a45d3ca56207fa901a56e6002434c1ce5
Joseane-Guedes <joseane_23@hotmail.com>
2021-08-01 00:09:34 -0300

    Add SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-
+import React from 'react'
+import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
 
 
@@ -17,8 +18,9 @@ export default function Home() {
           <p>Get acess to all the publications <br/>
           <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
-
+       
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
