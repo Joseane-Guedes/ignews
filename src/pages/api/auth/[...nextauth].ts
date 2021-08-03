60d4cecb9aae515c62888d5537be51d3d1c4f5eb
Joseane-Guedes <joseane_23@hotmail.com>
2021-08-02 22:38:13 -0300

    Bug Faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,36 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import { query as q } from "faunadb";
+
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user'
+      scope: "read:user",
     }),
   ],
-})
+  
+
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection("users"), 
+            { data: { email } }
+            )
+          );
+        return true;
+      } catch (err) {
+        console.log('fauna', err);
+        return false;
+      }
+    },
+  },
+});
