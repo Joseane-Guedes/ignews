805799a25f179f4ef733c179a96b585c88208ccb
Joseane-Guedes <joseane_23@hotmail.com>
2021-08-06 13:51:21 -0300

    Add posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Posts () {
+    return (
+        <>
+        <Head>
+            <title>Posts | Ignews</title>
+        </Head>
+
+        <main className={styles.container}>
+            <div className={styles.posts}>
+                <a>
+                <time> 06 de agosto de 2021 </time>
+                <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+                <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+                </a>
+                <a>
+                <time> 06 de agosto de 2021 </time>
+                <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+                <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+                </a>
+                <a>
+                <time> 06 de agosto de 2021 </time>
+                <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+                <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+                </a>
+            </div>
+        </main>
+        </>
+    );
+}
