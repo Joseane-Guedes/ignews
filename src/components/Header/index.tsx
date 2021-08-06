63df358d1184a0817ee40eea604d39825557f0f2
Joseane-Guedes <joseane_23@hotmail.com>
2021-08-06 19:52:05 -0300

    Add Link component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
-import styles from './styles.module.scss';
+import Link from 'next/link'
+
 import { SignInButton } from "./SignInButton";
+
+import Image from 'next/image'
 import logo from '../../../public/images/logo.svg';
+import styles from './styles.module.scss';
+
+
 
 export function Header() {
     return (
@@ -9,8 +14,12 @@ export function Header() {
             <div className={styles.headerContent}>
             <Image src={logo} alt="Ignews" />    
                 <nav>
+                    <Link href='/'>
                     <a className={styles.active}>Home</a>
+                    </Link>
+                    <Link href='/posts' prefetch>
                     <a >Posts</a>
+                    </Link>
                 </nav>
                 <SignInButton /> 
             </div>
