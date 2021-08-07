1b9abd3f472c4bcd4039ed8f82ff9028ee96cb05
Joseane-Guedes <joseane_23@hotmail.com>
2021-08-07 15:34:10 -0300

    Add Post and slug

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,10 +2,13 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from './styles.module.scss';
+
+
 
 type Post = {
     slug: string;
@@ -14,28 +17,30 @@ type Post = {
     updatedAt: string;
 }
 interface PostsProps {
-    posts: Post []
+    posts: Post[]
 }
 
-export default function Posts ({ posts }: PostsProps) {
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
-        <Head>
-            <title>Posts | Ignews</title>
-        </Head>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
 
-        <main className={styles.container}>
-            <div className={styles.posts}>
-            { posts.map(post =>(
-                <a key={post.slug} href="#">
-                <time>{post.updatedAt}</time>
-                <strong>{post.title}</strong>
-                <p>{post.excerpt}</p>
-                </a>
-            )) }    
-             </div>
-        </main>
-       </>
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    {posts.map(post => (
+                        <Link href={`/posts/${post.slug}`}>
+                            <a key={post.slug}>
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
+                        </Link>
+                    ))}
+                </div>
+            </main>
+        </>
     );
 }
 
@@ -50,10 +55,10 @@ export const getStaticProps: GetStaticProps = async () => {
     })
 
     const posts = response.results.map(post => {
-        return{
+        return {
             slug: post.uid,
             title: RichText.asText(post.data.title),
-            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ??'',
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
             updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
                 day: '2-digit',
                 month: 'long',
@@ -63,9 +68,9 @@ export const getStaticProps: GetStaticProps = async () => {
     });
 
     //console.log(JSON.stringify(response, null, 2)) DEBUG - objeto ou array com conteudo em cascata.
-    return { 
-        props:{ 
-            posts         
+    return {
+        props: {
+            posts
         }
     }
 }
