c344f0c39825a1443d1ccfbcdd98e9f908137463
Joseane-Guedes <joseane_23@hotmail.com>
2021-10-03 17:29:01 -0300

    :white_check_mark: test: changed jest method to check texts values on screen

---
+++ src/components/Header/SignInButton/SignInButton.spec.tsx
@@ -0,0 +1,37 @@
+import { SignInButton } from ".";
+import { useSession } from "next-auth/client";
+import { render, screen } from "@testing-library/react";
+import { mocked } from "ts-jest/utils";
+
+jest.mock("next-auth/client");
+
+describe("SignInButton component", () => {
+
+  it("it renders correctly when user is not authenticated", () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    render(<SignInButton />);
+
+    expect(screen.getByText("Sign in with GitHub")).toBeInTheDocument();
+  })
+
+  it("renders correctly when user is authenticated", () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValue([{
+        user: {
+          name: 'John Doe',
+          email: 'john.doe@example.com',
+        },
+        expires: 'fake-expires'
+      }, false
+    ])
+
+
+    render(<SignInButton />);
+
+    expect(screen.getByText("John Doe")).toBeInTheDocument();
+  });
+});
